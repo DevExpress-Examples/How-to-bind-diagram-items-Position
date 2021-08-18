f7892de8d359b6a2526cfde2852446fedf1a7ba3
DmitryRomenskiy <Dmitry.Romenskiy@DevExpress.com>
2021-08-18 12:18:07 +0300

    add example content

---
+++ CS/DXSample/Form1.cs
@@ -0,0 +1,38 @@
+﻿using DevExpress.Diagram.Core;
+using DevExpress.XtraDiagram;
+using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace DXSample {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+
+            ViewModel viewModel = new ViewModel();
+            diagramDataBindingController1.GenerateItem += DiagramDataBindingController1_GenerateItem;
+            diagramDataBindingController1.CustomLayoutItems += DiagramDataBindingController1_CustomLayoutItems;
+            diagramDataBindingController1.DataSource = viewModel.Items;
+            diagramDataBindingController1.KeyMember = "Id";
+            diagramDataBindingController1.ConnectorsSource = viewModel.Connections;
+            diagramDataBindingController1.ConnectorFromMember = "From";
+            diagramDataBindingController1.ConnectorToMember = "To";
+        }
+
+        private void DiagramDataBindingController1_CustomLayoutItems(object sender, DiagramCustomLayoutItemsEventArgs e) {
+            e.Handled = true;
+        }
+
+        private void DiagramDataBindingController1_GenerateItem(object sender, DiagramGenerateItemEventArgs e) {
+            var item = new DiagramShape {
+                X = 27,
+                Width = 75,
+                Height = 50,
+                Shape = BasicShapes.Rectangle
+            };
+            item.Bindings.Add(new DiagramBinding("Content", "PositionString"));
+            item.Bindings.Add(new DiagramBinding("Position", "Position", DiagramBindingMode.TwoWay));
+            e.Item = item;
+        }
+    }
+}
---
+++ CS/DXSample/ViewModel.cs
@@ -0,0 +1,53 @@
+﻿using DevExpress.Utils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace DXSample {
+    public class ViewModel {
+        public ViewModel() {
+            Items = new List<Item>();
+            for (int i = 0; i < 5; i++)
+                Items.Add(new Item { Id = i, Name = "Item " + i, Position = new PointFloat(i * 100, i * 200) });
+            Connections = new List<Link>();
+            for (int i = 0; i < 4; i++)
+                Connections.Add(new Link { From = Items[i].Id, To = Items[i + 1].Id });
+            Connections.Add(new Link { From = Items[4].Id, To = Items[0].Id });
+        }
+
+        public List<Link> Connections { get; set; }
+        public List<Item> Items { get; set; }
+    }
+
+    public class Item : INotifyPropertyChanged {
+        PointFloat position;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(object sender, PropertyChangedEventArgs e) {
+            PropertyChanged?.Invoke(sender, e);
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public PointFloat Position {
+            get => position; set {
+                if (position == value) {
+                    return;
+                }
+
+                position = value;
+                OnPropertyChanged(this, new PropertyChangedEventArgs(nameof(Position)));
+                OnPropertyChanged(this, new PropertyChangedEventArgs(nameof(PositionString)));
+            }
+        }
+
+        public string PositionString => Position.ToString();
+    }
+
+    public class Link {
+        public object From { get; set; }
+        public object To { get; set; }
+    }
+}
